d6aef28193b53ba92a50fcec23b6ac31f4b9f481
mistik9 <mistik9@icloud.com>
2023-05-15 17:17:41 +0600

    add midleware logger

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const {
 } = require('./routes/index');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
@@ -19,6 +20,8 @@ app.use(express.json());
 mongoose.connect(URL)
   .then(console.log('DB is connected'))
   .catch((err) => console.log(err));
+
+app.use(requestLogger);
 
 app.use('/signup', signupRouter);
 app.use('/signin', signinRouter);
@@ -31,6 +34,7 @@ app.use('/cards', cardRouter);
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
 });
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
