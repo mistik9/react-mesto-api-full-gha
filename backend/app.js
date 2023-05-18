976977ab2879183c0bc8f186eaae13d08246e568
mistik9 <mistik9@icloud.com>
2023-05-18 18:09:38 +0600

    add CORS

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,16 @@ app.use(function(req, res, next) {
       // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
       res.header('Access-Control-Allow-Origin', origin);
   }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
+  
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+      // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  } 
 
   next();
 }); 
