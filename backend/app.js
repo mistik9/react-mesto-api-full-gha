57af48144858ec6e3d6290b67f06dd852ba5df95
mistik9 <mistik9@icloud.com>
2023-05-19 14:37:39 +0600

    add midleware cors

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const {
 } = require('./routes/index');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
@@ -16,31 +17,7 @@ const URL = 'mongodb://127.0.0.1:27017/mestodb';
 const app = express();
 app.use(express.json());
 
-const allowedCors = [
-  'https://mistik9mesto.nomoredomains.monster/',
-  'https://api.mistik9mesto.nomoredomains.monster/',
-  'localhost:3000'
-];
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers; 
-      if (allowedCors.includes(origin)) {
-      // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-      res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
-  
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-      // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  } 
-
-  next();
-}); 
+app.use(cors);
 
 mongoose.connect(URL)
   .then(console.log('DB is connected'))
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,25 @@
+const allowedCors = [
+  'https://mistik9mesto.nomoredomains.monster/',
+  'https://api.mistik9mesto.nomoredomains.monster/',
+  'localhost:3000',
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+};
+
+module.exports = { cors };
