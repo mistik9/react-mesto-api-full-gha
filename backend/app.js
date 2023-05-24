f119d644a88a76e4ba099a202dd4e4775352a865
mistik9 <mistik9@icloud.com>
2023-05-24 18:13:58 +0600

    fix config errors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
