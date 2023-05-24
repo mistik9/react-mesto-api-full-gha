020e260c2c4fc2e08a4b6edb3416df16cd4400ab
mistik9 <mistik9@icloud.com>
2023-05-24 23:44:07 +0600

    fix eslint

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../utils/errors/AuthError');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt
+  const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token,  NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     req.user = payload;
   } catch (err) {
     throw new AuthError('Необходима авторизация');
