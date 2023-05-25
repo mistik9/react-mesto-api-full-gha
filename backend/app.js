d9ffdbc11b7411eb99e807fd24d27f67aab902a3
mistik9 <mistik9@icloud.com>
2023-05-25 22:23:36 +0600

    change port 3000

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const { cors } = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 const app = express();
